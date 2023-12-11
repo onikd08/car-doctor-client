afeff9c028e759538306f395ff6fce5458b954b0
Anik Das <onikd08@gmail.com>
2023-12-11 03:48:54 +0200

    Add navbar and footer

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../shared/Footer/Footer";
+import Navbar from "../shared/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="mx-auto max-w-screen-xl">
+      <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
